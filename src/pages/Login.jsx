637186afb75dba5e8d8b53f0aa5ccda8edfd62a4
Sunny <heysunny612@gmail.com>
2023-06-29 19:57:11 +0900

    배포수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import "../stylesheets/pages/Login.scss";
-import Button from "../components/Button/Button";
-import { BsGoogle } from "react-icons/bs";
+import React, { useEffect, useState } from 'react';
+import '../stylesheets/pages/Login.scss';
+import Button from '../components/Button/Button';
+import { BsGoogle } from 'react-icons/bs';
 import {
   createAccount,
   loginWithEamil,
   loginWithSocial,
-} from "../api/firebase";
-import { useNavigate, Navigate } from "react-router-dom";
-import { useUserContext } from "../context/UserContext";
+} from '../api/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useUserContext } from '../context/UserContext';
 
 export default function Login() {
-  const [account, setAccount] = useState({ email: "", password: "" });
+  const [account, setAccount] = useState({ email: '', password: '' });
   const [login, setLogin] = useState(true);
   const { user } = useUserContext();
   const navigate = useNavigate();
@@ -26,38 +26,38 @@ export default function Login() {
 
   useEffect(() => {
     if (user) {
-      navigate("/", { replace: true });
+      navigate('/', { replace: true });
     }
   }, [user]);
 
   return (
-    <div className="login_area">
-      <div className="login_box">
-        <h2>{login ? "LOGIN" : "Create Account with Email & password"}</h2>
-        <form className="form_login" onSubmit={handleSubmit}>
+    <div className='login_area'>
+      <div className='login_box'>
+        <h2>{login ? 'LOGIN' : 'Create Account with Email & password'}</h2>
+        <form className='form_login' onSubmit={handleSubmit}>
           <input
-            type="email"
-            placeholder="E-mail"
-            name="email"
+            type='email'
+            placeholder='E-mail'
+            name='email'
             value={account.email}
             onChange={handleChange}
           />
           <input
-            type="password"
-            placeholder="Password"
-            name="password"
+            type='password'
+            placeholder='Password'
+            name='password'
             value={account.password}
             onChange={handleChange}
           />
-          <Button type="submit">{login ? "LOGIN" : "CREATE ACCOUNT"}</Button>
+          <Button type='submit'>{login ? 'LOGIN' : 'CREATE ACCOUNT'}</Button>
         </form>
         <button
-          className="btn_sign_in"
+          className='btn_sign_in'
           onClick={() => setLogin((prev) => !prev)}
         >
-          {!login ? " Sign In" : "Create Account"}
+          {!login ? ' Sign In' : 'Create Account'}
         </button>
-        <Button type="white" onClick={loginWithSocial}>
+        <Button type='white' onClick={loginWithSocial}>
           Continue with Google <BsGoogle />
         </Button>
       </div>
